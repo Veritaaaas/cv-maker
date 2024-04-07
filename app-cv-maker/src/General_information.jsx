fcce59ed336c160f468dc3cffabd8823d136ae12
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-04-07 22:13:00 +0800

    readjust the structure of general info

--- app-cv-maker/src/General_information.jsx
+++ app-cv-maker/src/General_information.jsx
@@ -5,13 +5,14 @@ function General_information({ fullName, setFullName, email, setEmail, phoneNumb
         <div className="Personal-Information">
             <h1>Personal Information</h1>
             <label>Full Name</label>
-            <input type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} placeholder="Enter your full name" />
-            <label>Email</label>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
-            <label>Phone Number</label>
-            <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} placeholder="Enter your phone number" />
+            <input type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} placeholder="John Doe" />
             <label>Address</label>
-            <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="Enter your address" />
+            <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="City, PH" />
+            <label>Email</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="johndoe@gmail.com" />
+            <label>Phone Number</label>
+            <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} placeholder="012345679" />
+            
         </div>
     )
 }
